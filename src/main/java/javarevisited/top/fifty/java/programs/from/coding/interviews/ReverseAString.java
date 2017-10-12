f1c47f81f12d5419fbe2f81e1b653150a59f9029
Ahmed Tawila <atawila@yahoo.com>
2017-10-12 15:26:22 +0200

    reverse string optimization

--- src/main/java/javarevisited/top/fifty/java/programs/from/coding/interviews/ReverseAString.java
+++ src/main/java/javarevisited/top/fifty/java/programs/from/coding/interviews/ReverseAString.java
@@ -3,13 +3,13 @@ package javarevisited.top.fifty.java.programs.from.coding.interviews;
 public class ReverseAString {
 
 	public static String reverseString(String s) {
-		String result = "";
+		StringBuilder result = new StringBuilder();
 		for (int i = 0; i < s.length(); i++) {
-			result += s.charAt(s.length() - i - 1);
+			result.append(s.charAt(s.length() - i - 1));
 		}
-		return result;
+		return result.toString();
 	}
-	
+
 	public static void main(String[] args) {
 		String test = "To be reversed!";
 		System.out.println(reverseString(test));
